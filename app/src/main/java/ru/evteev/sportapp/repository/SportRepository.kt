2a6adf9268f22c04a65b51789ae6529f06add4bd
relt <haxrelt@gmail.com>
2023-12-15 00:46:15 +0300

    update SportRepository

--- app/src/main/java/ru/evteev/sportapp/repository/SportRepository.kt
+++ app/src/main/java/ru/evteev/sportapp/repository/SportRepository.kt
@@ -3,26 +3,15 @@ package ru.evteev.sportapp.repository
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room.databaseBuilder
+import ru.evteev.sportapp.database.SportDAO
 import ru.evteev.sportapp.database.SportDatabase
 import ru.evteev.sportapp.domain.Sport
 import java.util.UUID
 import java.util.concurrent.Executors
 
-class SportRepository private constructor(context: Context) {
-    private val database: SportDatabase = databaseBuilder<SportDatabase>(
-        context.applicationContext,
-        SportDatabase::class.java,
-        "sport"
-    ).build();
-
-    private val sportDao = database.sportDao()
-    private val executor = Executors.newSingleThreadExecutor()
+class SportRepository(private val sportDao: SportDAO) {
+    fun insertSport(sport: Sport) = sportDao.addSport(sport);
     fun getSports(): LiveData<List<Sport>> = sportDao.getSports();
     fun getSport(id: UUID): LiveData<Sport> = sportDao.getSport(id);
-
-    companion object {
-        private var INSTANCE: SportRepository? = null;
-    }
-
-
+    fun updateSport(sport: Sport) = sportDao.updateSport(sport);
 }
